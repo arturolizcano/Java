0ff8333af575b2b5daa95e540823d0dc56df90ec
Ivan Lizcano <arturolizcano@hotmail.com>
2015-05-27 22:16:09 -0700

    Add Activity 5

---
+++ Activity 5/Wget.java
@@ -0,0 +1,32 @@
+package paquetedemo1;
+
+/*************************************************************************
+ *  Compilation:  javac Wget.java
+ *  Execution:    java Wget url
+ *
+ *  Reads in a URL specified on the command line and saves its contents
+ *  in a file with the given name.
+ *
+ *  % java Wget http://introcs.cs.princeton.edu/java/data/codes.csv
+ *
+ *************************************************************************/
+
+public class Wget {
+
+    public static void main(String[] args) {
+
+        // read in data from URL
+        String url = args[0];
+        In in = new In(url);
+        String data = in.readAll();
+
+        // write data to a file
+        String filename = url.substring(url.lastIndexOf('/') + 1);
+        System.out.println(filename);
+        Out out = new Out(filename);
+        out.println(data);
+        System.out.println(data);
+        out.close();
+    }
+
+}
